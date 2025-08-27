dbf68ac055daff111c355aad93c6ad66f3ace29c
Bhavin Giniya <bhavin@sarvadhi.com>
2025-08-27 11:50:12 +0530

    add env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import { API_BASE_URL } from "../config";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  // baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
